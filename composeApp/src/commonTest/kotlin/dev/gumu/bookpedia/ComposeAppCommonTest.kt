458a75721f279ddc94ece71305971fe0cc6a7c8c
Gustavo Müller <gumu96.dev@gmail.com>
2026-03-24 11:57:31 -0600

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/dev/gumu/bookpedia/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package dev.gumu.bookpedia
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/dev/gumu/bookpedia/MainViewController.kt
@@ -0,0 +1,5 @@
+package dev.gumu.bookpedia
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
